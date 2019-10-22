370d2bc6f2a85064c70595e1a23c8d49861ddfb3
ramigs <ramigs@gmail.com>
2019-10-22 12:49:37 +0100

    All Alerts implemented

--- src/components/Container.js
+++ src/components/Container.js
@@ -8,27 +8,54 @@ export default () => {
 		setItems
 	] = useState([]);
 	const [
-		show,
-		setShow
+		showSuccessAdded,
+		setShowSuccessAdded
 	] = useState(false);
-	const deleteCard = (id) => {
-		setItems(items.filter((item) => item.id !== id));
+	const [
+		showSuccessDeleted,
+		setShowSuccessDeleted
+	] = useState(false);
+	const [
+		showWarningExists,
+		setShowWarningExists
+	] = useState(false);
+	const [
+		itemDeleted,
+		setItemDeleted
+	] = useState('');
+	const deleteCard = (i) => {
+		setItems(items.filter((item) => item.id !== i.id));
+		setShowSuccessAdded(false);
+		setShowWarningExists(false);
+		setShowSuccessDeleted(true);
+		setItemDeleted(i.name);
 	};
 
 	const addItem = (item) => {
-		if (items.find((aux) => aux.name === item.name)) alert('this city already exists');
-		else {
+		if (items.find((aux) => aux.name === item.name)) {
+			setShowSuccessAdded(false);
+			setShowSuccessDeleted(false);
+			setShowWarningExists(true);
+		} else {
 			setItems([
 				...items,
 				item
 			]);
-			setShow(true);
+			setShowSuccessDeleted(false);
+			setShowWarningExists(false);
+			setShowSuccessAdded(true);
 		}
 	};
 	return (
 		<div className="container">
-			<Alert show={show} variant="success" onClose={() => setShow(false)} dismissible>
-				City added!
+			<Alert show={showSuccessAdded} variant="success" onClose={() => setShowSuccessAdded(false)} dismissible>
+				City {items[items.length - 1] ? items[items.length - 1].name : ''} added!
+			</Alert>
+			<Alert show={showSuccessDeleted} variant="success" onClose={() => setShowSuccessDeleted(false)} dismissible>
+				City {itemDeleted} deleted!
+			</Alert>
+			<Alert show={showWarningExists} variant="warning" onClose={() => setShowWarningExists(false)} dismissible>
+				City already exists!
 			</Alert>
 			<Search onSearch={addItem} />
 			<h4>
@@ -39,7 +66,7 @@ export default () => {
 				<div className="row padding">
 					{items.map((item) => (
 						<div key={item.id} className="col-4 col-6 col-12">
-							<WeatherCard onDelete={() => deleteCard(item.id)} key={item.id} item={item} />
+							<WeatherCard onDelete={() => deleteCard(item)} key={item.id} item={item} />
 						</div>
 					))}
 				</div>
