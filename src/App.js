84799322e8526e9f43d713f409538f7d9253ee2f
ramigs <ramigs@gmail.com>
2019-10-22 10:02:24 +0100

    key={item.id} added to fix key warning. React Bootstrap Success Alert added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Header from './components/Header';
 import Container from './components/Container';
 import './assets/css/App.css';
 
 function App () {
-	const Fragment = React.Fragment;
 	return (
 		<Fragment>
 			<Header />
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
 import Search from './Search';
 import WeatherCard from './WeatherCard';
 export default () => {
@@ -6,28 +7,38 @@ export default () => {
 		items,
 		setItems
 	] = useState([]);
+	const [
+		show,
+		setShow
+	] = useState(false);
 	const deleteCard = (id) => {
 		setItems(items.filter((item) => item.id !== id));
 	};
 
 	const addItem = (item) => {
 		if (items.find((aux) => aux.name === item.name)) alert('this city already exists');
-		else
+		else {
 			setItems([
 				...items,
 				item
 			]);
+			setShow(true);
+		}
 	};
 	return (
 		<div className="container">
+			<Alert show={show} variant="success" onClose={() => setShow(false)} dismissible>
+				City added!
+			</Alert>
 			<Search onSearch={addItem} />
 			<h4>
 				Total Cities <span className="badge badge-info">{items.length}</span>
 			</h4>
 			<div className="cards-list">
+				<div className="row padding" />
 				<div className="row padding">
 					{items.map((item) => (
-						<div className="col-4 col-6 col-12">
+						<div key={item.id} className="col-4 col-6 col-12">
 							<WeatherCard onDelete={() => deleteCard(item.id)} key={item.id} item={item} />
 						</div>
 					))}
