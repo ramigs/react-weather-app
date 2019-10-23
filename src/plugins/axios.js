e66bfc15903bf31eed5eab8ae50ddd5d393a974c
ramigs <ramigs@gmail.com>
2019-10-23 16:51:45 +0100

    Updating to https

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
 const axios = Axios.create({
-	baseURL : `http://api.openweathermap.org/data/2.5/weather?appid=${process.env
+	baseURL : `https://api.openweathermap.org/data/2.5/weather?appid=${process.env
 		.REACT_APP_WEATHER_API_KEY}&units=metric`
 });
 
